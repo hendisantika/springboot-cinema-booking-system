d3a8487953f9f41da4feedee042aab773258d229
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-20 06:10:05 +0700

    #7 - Add FilmSession Model

---
+++ src/main/java/com/hendisantika/cinemabookingsystem/model/FilmSession.java
@@ -0,0 +1,40 @@
+package com.hendisantika.cinemabookingsystem.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import sun.security.krb5.internal.Ticket;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/04/20
+ * Time: 06.09
+ */
+@Data
+@Entity
+@EqualsAndHashCode(callSuper = true)
+public class FilmSession extends BaseEntity {
+
+    private LocalDateTime time;
+
+    @ManyToOne
+    @JoinColumn(name = "film_id")
+    private Film film;
+
+    @ManyToOne
+    @JoinColumn(name = "hall_id")
+    private Hall hall;
+
+    @OneToMany(mappedBy = "filmSession")
+    private Set<Ticket> tickets;
+}
