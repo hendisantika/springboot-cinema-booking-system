39386fdaebf540ac02fd79089adac5eb13bbe845
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-20 06:02:08 +0700

    #1 - Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot-cinema-booking-system
---
+++ src/test/java/com/hendisantika/cinemabookingsystem/CinemaBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.cinemabookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
