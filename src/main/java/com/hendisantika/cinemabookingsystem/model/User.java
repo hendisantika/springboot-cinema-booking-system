201891ab119eb24f22998ee316e2d2e68971ddbe
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-20 06:11:49 +0700

    #10 - Add User Model

---
+++ src/main/java/com/hendisantika/cinemabookingsystem/model/User.java
@@ -0,0 +1,45 @@
+package com.hendisantika.cinemabookingsystem.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import sun.security.krb5.internal.Ticket;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/04/20
+ * Time: 06.11
+ */
+@Data
+@Entity
+@EqualsAndHashCode(callSuper = true)
+public class User extends BaseEntity {
+
+    private String username;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phone;
+
+    private String password;
+
+    private String email;
+
+    @OneToMany(mappedBy = "user")
+    private Set<Ticket> tickets;
+
+    @ManyToOne
+    @JoinColumn(name = "discount_id")
+    private Discount discount;
+}
