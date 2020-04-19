97f23de9e5ec890b34f234d3597b0580ba703258
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-20 06:32:32 +0700

    #32 - Add EntityUtils

---
+++ src/main/java/com/hendisantika/cinemabookingsystem/util/EntityUtils.java
@@ -0,0 +1,16 @@
+package com.hendisantika.cinemabookingsystem.util;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/04/20
+ * Time: 06.31
+ */
+@UtilityClass
+public class EntityUtils {
+}
