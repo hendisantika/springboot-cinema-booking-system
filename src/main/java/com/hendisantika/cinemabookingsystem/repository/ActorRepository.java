88b8beb44614e888bf82dcf2ccdd12b554a953d4
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-20 06:14:53 +0700

    #15 - Add ActorRepository

---
+++ src/main/java/com/hendisantika/cinemabookingsystem/repository/ActorRepository.java
@@ -0,0 +1,16 @@
+package com.hendisantika.cinemabookingsystem.repository;
+
+import com.hendisantika.cinemabookingsystem.model.Actor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/04/20
+ * Time: 06.14
+ */
+public interface ActorRepository extends JpaRepository<Actor, Long> {
+}
