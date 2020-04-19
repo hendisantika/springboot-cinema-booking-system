7424382eb29890afb4007036989cfed3d70dc66e
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-20 06:33:46 +0700

    #34 - Add safeGetEntity, getDummyEntityById & safeGetEntityCollection Util in EntityUtils

--- src/main/java/com/hendisantika/cinemabookingsystem/util/EntityUtils.java
+++ src/main/java/com/hendisantika/cinemabookingsystem/util/EntityUtils.java
@@ -24,4 +24,25 @@ public class EntityUtils {
     public static <T extends BaseEntity> List<T> safeGetEntityCollection(List<T> entityCollection, String msg) {
         return CommonUtils.safeGet(entityCollection, () -> new EntityNotFoundException(msg));
     }
+
+    public static <T extends BaseEntity> T safeGetEntity(T object) {
+        return safeGetEntity(object, "Entity not found");
+    }
+
+    public static <T extends BaseEntity> List<T> safeGetEntityCollection(List<T> entityCollection) {
+        return safeGetEntityCollection(entityCollection, "Entities collection is empty");
+    }
+
+    public static <T extends BaseEntity> T getDummyEntityById(Long id, Class<T> entityClass) {
+        T entity;
+
+        try {
+            entity = entityClass.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+        entity.setId(id);
+        return entity;
+    }
 }
