dad3e6913e87c7a267a63f32e80f9eb3aaac1e70
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-20 06:33:11 +0700

    #33 - Add safeGetEntity & safeGetEntityCollection Util in EntityUtils

--- src/main/java/com/hendisantika/cinemabookingsystem/util/EntityUtils.java
+++ src/main/java/com/hendisantika/cinemabookingsystem/util/EntityUtils.java
@@ -1,6 +1,10 @@
 package com.hendisantika.cinemabookingsystem.util;
 
+import com.hendisantika.cinemabookingsystem.model.BaseEntity;
 import lombok.experimental.UtilityClass;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,4 +17,11 @@ import lombok.experimental.UtilityClass;
  */
 @UtilityClass
 public class EntityUtils {
+    public static <T extends BaseEntity> T safeGetEntity(T object, String msg) {
+        return CommonUtils.safeGet(object, () -> new EntityNotFoundException(msg));
+    }
+
+    public static <T extends BaseEntity> List<T> safeGetEntityCollection(List<T> entityCollection, String msg) {
+        return CommonUtils.safeGet(entityCollection, () -> new EntityNotFoundException(msg));
+    }
 }
