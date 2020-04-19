2674a68f469e930d7a74dc296c7b3269d1ddb289
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-20 06:10:49 +0700

    #9 - Add Hall Model

---
+++ src/main/java/com/hendisantika/cinemabookingsystem/model/Hall.java
@@ -0,0 +1,40 @@
+package com.hendisantika.cinemabookingsystem.model;
+
+import com.sun.rowset.internal.Row;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/04/20
+ * Time: 06.09
+ */
+@Data
+@Entity
+@EqualsAndHashCode(callSuper = true)
+public class Hall extends BaseEntity {
+
+    private String title;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "cinema_id")
+    private Cinema cinema;
+
+    @OneToMany(mappedBy = "hall")
+    private Set<Row> rows;
+
+    @OneToMany(mappedBy = "hall")
+    private Set<FilmSession> filmSessions;
+}
