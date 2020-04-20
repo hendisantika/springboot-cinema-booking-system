6c2adc7af8d9eefcde8e3f1e4df8a07ce1347eb8
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-21 06:12:27 +0700

    #64 - Add loadUserByUsername service in UserDetailsServiceImpl

--- src/main/java/com/hendisantika/cinemabookingsystem/service/UserDetailsServiceImpl.java
+++ src/main/java/com/hendisantika/cinemabookingsystem/service/UserDetailsServiceImpl.java
@@ -1,9 +1,13 @@
 package com.hendisantika.cinemabookingsystem.service;
 
+import com.hendisantika.cinemabookingsystem.model.User;
 import com.hendisantika.cinemabookingsystem.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,5 +26,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     public UserDetailsServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), null);
     }
 }
