44b78f4ca4b81d53ee75703c87b43cba7bcecf10
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-21 07:53:58 +0700

    #71 - Add WebSecurityConfig

---
+++ src/main/java/com/hendisantika/cinemabookingsystem/config/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.hendisantika.cinemabookingsystem.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/04/20
+ * Time: 07.53
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public WebSecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+}
