aaf66379399a191091f597fbd9d62b1c450a22a2
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-24 09:45:32 +0700

    #137 - Add allActorUser API in ActorController

--- src/main/java/com/hendisantika/cinemabookingsystem/controller/ActorController.java
+++ src/main/java/com/hendisantika/cinemabookingsystem/controller/ActorController.java
@@ -1,10 +1,13 @@
 package com.hendisantika.cinemabookingsystem.controller;
 
+import com.hendisantika.cinemabookingsystem.model.Actor;
 import com.hendisantika.cinemabookingsystem.service.ActorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,5 +32,12 @@ public class ActorController {
     public String allActor(Model model) {
         model.addAttribute("actors", actorService.getAllActors());
         return "/admin/actor";
+    }
+
+    @GetMapping(value = "/actor")
+    public String allActorUser(@RequestParam(required = false, defaultValue = "1") Integer page, Model model) {
+        Page<Actor> pages = actorService.getAllActorsPage(page);
+        model.addAttribute("allActor", pages);
+        return "/actor";
     }
 }
