92965afdedfd561da553c6e4bf819be71cb9dea3
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-24 10:01:42 +0700

    #150 - Add DiscountController

---
+++ src/main/java/com/hendisantika/cinemabookingsystem/controller/DiscountController.java
@@ -0,0 +1,25 @@
+package com.hendisantika.cinemabookingsystem.controller;
+
+import com.hendisantika.cinemabookingsystem.service.DiscountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cinema-booking-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/04/20
+ * Time: 10.01
+ */
+@Controller
+public class DiscountController {
+
+    private final DiscountService discountService;
+
+    @Autowired
+    public DiscountController(DiscountService discountService) {
+        this.discountService = discountService;
+    }
+}
