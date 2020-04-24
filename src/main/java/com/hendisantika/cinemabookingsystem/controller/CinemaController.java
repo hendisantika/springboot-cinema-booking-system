2d0354d3baa8c6c61c742df71d369f69e25473e9
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-24 09:57:52 +0700

    #144 - Add allCinema API in CinemaController

--- src/main/java/com/hendisantika/cinemabookingsystem/controller/CinemaController.java
+++ src/main/java/com/hendisantika/cinemabookingsystem/controller/CinemaController.java
@@ -3,6 +3,8 @@ package com.hendisantika.cinemabookingsystem.controller;
 import com.hendisantika.cinemabookingsystem.service.CinemaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,5 +20,11 @@ public class CinemaController {
 
     @Autowired
     private CinemaService cinemaService;
+
+    @GetMapping(value = "/admin/cinema")
+    public String allCinema(Model model) {
+        model.addAttribute("cinemas", cinemaService.getAllCinemas());
+        return "/admin/cinema";
+    }
 
 }
