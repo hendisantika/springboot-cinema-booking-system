cb18833b5ee99745e1ddef8443aa13b1e1c3600b
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-26 04:12:22 +0700

    #186 - Add login API in LoginController

--- src/main/java/com/hendisantika/cinemabookingsystem/controller/LoginController.java
+++ src/main/java/com/hendisantika/cinemabookingsystem/controller/LoginController.java
@@ -44,4 +44,15 @@ public class LoginController {
         return "/index";
     }
 
+    @GetMapping(value = "/login")
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("error", "Your username and password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "/login";
+    }
+
 }
