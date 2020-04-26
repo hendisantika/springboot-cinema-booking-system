308e85379e0679298d92675f2b04434edb590caa
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-27 04:17:21 +0700

    #216 - Bug Fix UserController

--- src/main/java/com/hendisantika/cinemabookingsystem/controller/UserController.java
+++ src/main/java/com/hendisantika/cinemabookingsystem/controller/UserController.java
@@ -1,12 +1,20 @@
 package com.hendisantika.cinemabookingsystem.controller;
 
+import com.hendisantika.cinemabookingsystem.model.Discount;
+import com.hendisantika.cinemabookingsystem.model.User;
 import com.hendisantika.cinemabookingsystem.service.DiscountService;
 import com.hendisantika.cinemabookingsystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 /**
  * Created by IntelliJ IDEA.
